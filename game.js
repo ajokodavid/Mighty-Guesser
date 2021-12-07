69d1554cd4e37f477f188c5bedef33de9fb660e8
Dave <58633928+ajokodavid@users.noreply.github.com>
2021-12-07 20:32:29 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,32 @@
+let userScore = 0;
+let computerScore = 0;
+let chances = 0;
+let userInput = document.getElementById("inputer");
+let userInput2 = document.getElementById("result");
+let computer_score = document.querySelector("#computerscore");
+let user_score = document.querySelector("#userscore");
+let userChances = document.getElementById("user_chances");
+
+function playGame() {
+  let computerGuess = Math.floor(Math.random() * 10);
+
+  if (userInput.value == computerGuess) {
+    user_score.innerHTML = userScore++;
+    userChances.innerHTML = chances++;
+    userInput2.innerHTML = "You are correct!";
+  } else if (userInput.value > 9 && userInput.value < 0) {
+    userInput2.innerHTML = "Please enter a number between 0 and 9";
+  } else {
+    computer_score.innerHTML = computerScore++;
+    userChances.innerHTML = chances++;
+    userInput2.innerHTML = "You are wrong!";
+  }
+
+  if(chances == 20){
+    userInput.style.display = "none";
+    userInput2.innerHTML = "Game Over!";
+    userChances.innerHTML = "";
+  }
+}
+
+userInput.addEventListener("keypress", playGame);
